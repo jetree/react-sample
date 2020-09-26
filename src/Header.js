03f5fbbfbe7f0f7e56c858d85dbea504f76da726
神谷友樹 <jetree900@gmail.com>
2020-09-27 08:45:37 +0900

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="bg-dark">
+        <nav>
+          <h1 className="text-light pl-3 mb-0">Agri-Eight</h1>
+        </nav>
+      </header>
+    );}
+}
+
+export default Header
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-danger">
+          aaaa
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Main
