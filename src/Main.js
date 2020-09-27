21ff97938e8ae86e13467eb4d158601c4448dc2a
神谷友樹 <jetree900@gmail.com>
2020-09-27 09:13:16 +0900

    サンプルのグラフを表示

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import Chart from "./Chart"
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <div className="bg-danger">
-          aaaa
-        </div>
+      <div className="container">
+        <Chart />
       </div>
     )
   }
