78b7a5e447b4d470e4205f1eab0c9fe491652c8b
神谷友樹 <jetree900@gmail.com>
2020-10-04 21:20:15 +0900

    axiosを導入もcorsでつまづく

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react"
+import axios from 'axios'
 import Chart from "./Chart"
 
 class Main extends Component {
+  getData() {
+    const data = axios.get('raspberrypi.local:8081/conditions')
+    return data
+  }
+
   render() {
     return (
       <div className="container">
+        <div className="container text-right my-2">
+          <button onClick={this.getData} type="button" className="btn btn-primary">取得</button>
+        </div>
         <Chart />
       </div>
     )
