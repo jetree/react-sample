9a201df4ec3c5052a30850af3abf4e5ec1ac2ec1
神谷友樹 <jetree900@gmail.com>
2020-10-18 16:56:20 +0900

    ラズパイから取得したデータを使ってグラフの描画をできる様にした

--- src/Main.js
+++ src/Main.js
@@ -2,10 +2,47 @@ import React, { Component } from "react"
 import axios from 'axios'
 import Chart from "./Chart"
 
+// axios.defaults.headers.post["Content-Type"] =  "application/x-www-form-urlencoded";
+
+
 class Main extends Component {
-  getData() {
-    const data = axios.get('raspberrypi.local:8081/conditions')
-    return data
+  constructor(props) {
+    super(props)
+    this.state = {
+      tempGraph: {
+        //   labels: [],
+        //   datasets: [{
+        //     data: [],
+        //     label: "",
+        //   }]
+        // },
+      },
+    }
+    this.getData = this.getData.bind(this)
+  }
+
+  async getData(){
+    const data = await axios.get('/conditions')
+    // const data = axios.get('/conditions')
+    const times = data.data.map(data => data.create_at)
+    const temp = data.data.map((data) => data.temp);
+    const humid = data.data.map((data) => data.humid);
+    const CPU_temp = data.data.map((data) => data.CPU_temp);
+    this.setState((state)=>{
+      return {
+        tempGraph: {
+          labels: times,
+          datasets: [{
+            data: temp,
+            label: "温度"
+          }]
+        },
+        times: times,
+        temp: temp,
+        humid: humid,
+        CPU_temp: CPU_temp
+      }
+    })
   }
 
   render() {
@@ -14,7 +51,7 @@ class Main extends Component {
         <div className="container text-right my-2">
           <button onClick={this.getData} type="button" className="btn btn-primary">取得</button>
         </div>
-        <Chart />
+        <Chart data = {this.state.tempGraph} />
       </div>
     )
   }
